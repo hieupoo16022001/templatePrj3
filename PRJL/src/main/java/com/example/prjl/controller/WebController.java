2dab8bdd7d49b3cb178ef24733a38442327056b6
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2021-08-03 14:42:09 +0700

    1

---
+++ PRJL/src/main/java/com/example/prjl/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.example.prjl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/Home")
+    public String Home(Model model) {
+        model.addAttribute("message", "HelloHome");
+        return "Home";
+    }
+}
+
---
+++ PRJL/src/test/java/com/example/prjl/PrjlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
